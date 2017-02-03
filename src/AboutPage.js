60c3284b852cc473924933a8edfb3d8d89cf0fa3
Liam McLennan <liam.mclennan@gmail.com>
2017-02-03 21:47:15 +1000

    create-react-app with router and redux

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import Link from 'react-router/lib/Link'
 
-export function AboutPageFactory(store) {
-    return function AboutPage(props) {
-        return <div>About <Link to="/">Home</Link></div>;
+export function AboutReducer(state = {data:{}}, action) {
+    switch (action.type) {
+        case 'STUFF_FROM_WORLD_BANK':
+            return { data: action.data };
+        default: return state;
     }
 }
+
+export function AboutPageFactory(store, sideEffects) {
+    return class AboutPage extends React.Component {
+        render() {
+            var data = store.getState().data;
+            return <div>{ data['user-agent'] ? data['user-agent'] : 'Loading...'}</div>;       
+        }
+        constructor(props) {
+            super(props);
+            sideEffects.onLoad();
+        }
+    }
+}
+
+export function aboutSideEffectsFactory(store) {
+    return { 
+        onLoad: function () {
+            fetch('https://httpbin.org/user-agent')
+                .then(response => response.json()).then(data => store.dispatch({type:'STUFF_FROM_WORLD_BANK',data:data}));
+        }
+    };
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './Layout';
-import {AboutPageFactory} from './AboutPage';
+import { AboutPageFactory, AboutReducer, aboutSideEffectsFactory } from './AboutPage';
 import './index.css';
 import Route from 'react-router/lib/Route'
 import Router from 'react-router/lib/Router'
 import { browserHistory } from 'react-router'
+import { createStore } from 'redux'
 
-ReactDOM.render((
-  <Router history={browserHistory} >
-    <Route path="/" component={Layout}>
-      <Route path="about" component={AboutPageFactory({})} />
-    </Route>
-  </Router>
-), document.getElementById('root'));
+let store = createStore(AboutReducer);
+let routes =  <Route path="/" component={Layout}>
+                <Route path="about" component={AboutPageFactory(store, aboutSideEffectsFactory(store))} />
+              </Route>;
+
+function render() {
+  return ReactDOM.render((
+    <Router history={browserHistory} >
+      {routes}
+    </Router>
+  ), document.getElementById('root'))
+}
+
+store.subscribe(render);
+render();
